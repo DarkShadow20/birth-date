c7d43a17f9f7ee3046b9d81e446f651afc61a1cb
DarkShadow20 <kunal8011@gmail.com>
2021-05-29 17:44:17 +0530

    fix:declarations

--- birth-prime.js
+++ birth-prime.js
@@ -1,26 +1,27 @@
-var chalk=require('chalk');
-var readlinesync=require('readline-sync');
+const chalk=require('chalk');
+const readlinesync=require('readline-sync');
 
-var userName=readlinesync.question(chalk.bold.cyan("May I know your name?"));
-var birthDate=readlinesync.question(chalk.bold.magenta("Hi "+userName+ " may I know your date of birth ?(dd-mm)"));
+const userName=readlinesync.question(chalk.bold.cyan("May I know your name?"));
+const birthDate=readlinesync.question(chalk.bold.magenta("Hi "+userName+ " may I know your date of birth ?(dd-mm)"));
 
 
 if (birthDate.length>5){
   console.log('Please try again');
 }
-var prime= birthDate.split("-");
+const prime= birthDate.split("-");
+
 if(prime[0]>31 || prime[1]>12){
   console.log(chalk.bold.red("Try Again"));
 }
 if(prime[1]==2 && prime[0]>29){
   console.log(chalk.bold.red("Try Again"));
 }
-var one=0;
-var sum=0;
-var two=0;
-var isPrime=true,reply;
-var numberOne=parseInt(prime[1]);
-var numberTwo=parseInt(prime[0])
+let one=0;
+let sum=0;
+let two=0;
+let isPrime=true,reply;
+let numberOne=parseInt(prime[1]);
+let numberTwo=parseInt(prime[0])
 while(numberOne!=0 || numberTwo!=0){
   one=parseInt(numberOne%10);
   numberOne=parseInt(numberOne/10);
@@ -28,7 +29,6 @@ while(numberOne!=0 || numberTwo!=0){
   numberTwo=parseInt(numberTwo/10);
   sum=parseInt(sum+one+two);
 }
-
 for(let i=2;i<sum-1;i++){
   if(sum%i==0){
     isPrime=false;
